c2975c9fcec5d5025b6ad41afa40ccd3e1bbdd77
thaiguer <thaiguer.k@gmail.com>
2025-11-04 23:03:40 -0300

    remove .net maui. This thing stinks

--- src/SurfRater.Maui/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using Fonts;
-global using SurfRater.Maui.Data;
-global using SurfRater.Maui.PageModels;
-global using SurfRater.Maui.Pages;
-global using SurfRater.Maui.Services;
-global using SurfRater.Maui.Utilities;
--- src/SurfRater.Maui/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "SurfRater.Maui")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "SurfRater.Maui.Pages")]
