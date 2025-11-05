54948fbca8e00927cb7533c205fd46d2d7a11612
thaiguer <thaiguer.k@gmail.com>
2025-11-04 22:08:39 -0300

    add samplenotifier

---
+++ src/SurfRater.Maui/Notifications/SampleNotifier.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+
+namespace SurfRater.Maui.Notifications;
+
+internal class SampleNotifier
+{
+    internal async Task ShowNoticationAsync()
+    {
+        await Toast.Make("Hello from .NET MAUI!", ToastDuration.Short).Show();
+    }
+}
