bcb683c48c245bda67951dc071855c9ca8ed940d
thaiguer <thaiguer.k@gmail.com>
2025-11-09 16:33:22 -0300

    clean up

--- src/backend/SurfRater.Core/Data/Interfaces/IForecastApiConsult.cs
+++
@@ -1,8 +0,0 @@
-﻿using SurfRater.Core.Data.ValueObjects;
-
-namespace SurfRater.Core.Data.Interfaces;
-
-public interface IForecastApiConsult
-{
-    Coordinate Coordinate { get; set; }
-}
--- src/backend/SurfRater.Core/Data/Interfaces/IWeatherForecast.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace SurfRater.Core.Data.Interfaces;
-
-public interface IWeatherForecast : IForecastApiConsult
-{
-
-}
