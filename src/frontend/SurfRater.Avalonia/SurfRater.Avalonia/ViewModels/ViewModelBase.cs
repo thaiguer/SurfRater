efd5c74a087fd74bbcde295ab646e550e3b3f370
thaiguer <thaiguer.k@gmail.com>
2025-11-11 22:04:28 -0300

    add empty avalonia project

---
+++ src/frontend/SurfRater.Avalonia/SurfRater.Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SurfRater.Avalonia.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ src/frontend/SurfRater.Avalonia/SurfRater.Avalonia/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SurfRater.Avalonia.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
