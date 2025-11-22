136b7120dd6ba0fe17628193fde6e9f0ab565021
thaiguer <thaiguer.k@gmail.com>
2025-11-22 00:00:39 -0300

    add view

---
+++ src/SurfRater.Avalonia/Views/MyBeachesView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SurfRater.Avalonia;
+
+public partial class MyBeachesView : UserControl
+{
+    public MyBeachesView()
+    {
+        InitializeComponent();
+    }
+}
