96441f6f5eb85a4950faa8b0e6422c64efdeeed5
thaiguer <thaiguer.k@gmail.com>
2025-11-23 20:55:35 -0300

    theme properties

--- src/SurfRater.Avalonia/ViewModels/SettingsViewModel.cs
+++ src/SurfRater.Avalonia/ViewModels/SettingsViewModel.cs
@@ -1,8 +1,23 @@
-﻿using SurfRater.Avalonia.ViewModels.Base;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using SurfRater.Avalonia.ViewModels.Base;
 
 namespace SurfRater.Avalonia.ViewModels;
 
-public class SettingsViewModel : ViewModelBase
+public partial class SettingsViewModel : ViewModelBase
 {
+    [ObservableProperty]
+    private bool _useDarkTheme = false;
 
+    [ObservableProperty]
+    private bool _useLightTheme = false;
+
+    [ObservableProperty]
+    private bool _useSystemTheme = true;
+
+    [RelayCommand]
+    private void SetTheme()
+    {
+
+    }
 }
