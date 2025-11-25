ab7f77b66881c9ead8674f32806b6acc2046690d
thaiguer <thaiguer.k@gmail.com>
2025-11-25 11:13:47 -0300

    add content

--- src/SurfRater.Avalonia/ViewModels/AboutViewModel.cs
+++ src/SurfRater.Avalonia/ViewModels/AboutViewModel.cs
@@ -1,10 +1,7 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using SurfRater.Avalonia.ViewModels.Base;
+﻿using SurfRater.Avalonia.ViewModels.Base;
 
 namespace SurfRater.Avalonia.ViewModels;
 
 public partial class AboutViewModel : ViewModelBase
 {
-    [ObservableProperty]
-    private string _greeting = "This is About page!";
 }
