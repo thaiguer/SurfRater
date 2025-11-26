28cce9df0dbd92a273a3081c291e305796fc0c9c
thaiguer <thaiguer.k@gmail.com>
2025-11-26 10:40:11 -0300

    clean up

--- src/SurfRater.Avalonia/Views/HomeView.axaml.cs
+++ src/SurfRater.Avalonia/Views/HomeView.axaml.cs
@@ -1,8 +1,4 @@
 using Avalonia.Controls;
-using Avalonia.Controls.Notifications;
-using Avalonia.Interactivity;
-using Avalonia.VisualTree;
-using SurfRater.Avalonia.Services;
 
 namespace SurfRater.Avalonia.Views;
 
